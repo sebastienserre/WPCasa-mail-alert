dee8cc8b16004d5b3dd9eb2fad78727953741416
sebastienserre <sebastien@thivinfo.com>
2019-02-18 17:12:38 +0100

    add listing choice in widget

--- pro/inc/wpcasama-widget-pro-functions.php
+++ pro/inc/wpcasama-widget-pro-functions.php
@@ -1,14 +1,44 @@
 <?php
-	
+
 	if ( ! defined( 'ABSPATH' ) ) {
 		exit;
 	}
-	
+
 	add_action( 'wpcasama_end_widget', 'wpcasama_pro_add_filters' );
 	function wpcasama_pro_add_filters() {
-		
+
 		$details = wpsight_details();
-		
+
+		/**
+		 * Get Listing Type
+		 */
+		$listing_type = wpsight_get_option('wpcasama_pro_type');
+
+		if ( '1' === $listing_type ){
+			$type_List = get_terms( 'listing-type', array(
+					'hide_empty' => false,
+					)
+			);
+
+			foreach ( $type_List as $list ){
+				$types[ $list->term_id ] = $list->name;
+			}
+
+			?>
+			<label for="wpcasama_type"><?php _e( 'Listing Type:', 'wpcasa-mail-alert-pro' ) ?></label>
+			<select name="wpcasama_type">
+				<?php
+				foreach ( $types as $key => $value ){
+					?>
+					<option value="<?php echo $key; ?>"><?php echo $value; ?></option>
+					<?php
+				}
+				?>
+			</select>
+			<?php
+		}
+
+
 		foreach ( $details as $key => $detail ) {
 			$key = wpsight_get_option( 'wpcasama_pro_' . $detail['label'] );
 			if ( ! empty( $detail['unit'] ) ) {
@@ -16,7 +46,7 @@
 			} else {
 				$unit = '';
 			}
-			
+
 			if ( $key == "1" ) { ?>
                 <div class="wpcasama-widget-field">
                     <label for="thfo_mailalert_<?php echo $detail['id']; ?>"> <?php echo $detail['label'] . $unit ?></label>
@@ -24,15 +54,15 @@
                            name="thfo_mailalert_<?php echo $detail['id']; ?>" type="number" step="1" value="0" min="0"/>
                 </div>
 			<?php }
-			
+
 		}
 		$offer = wpsight_get_option( 'wpcasama_pro_type' );
-		
+
 		if ( $offer == '1' ) {
 			add_action( 'wpcasama_end_widget', 'wpcasama_add_filter_offer' );
 		}
 	}
-	
+
 	function wpcasama_add_filter_offer() {
 		?>
         <div class="wpcasama-widget-field">
@@ -46,10 +76,10 @@
         </div>
 		<?php
 	}
-	
+
 	add_filter( 'wpcasama/pro/save/meta', 'wpcasama_save' );
 	function wpcasama_save( $params ) {
-		
+
 		$details1 = sanitize_text_field( $_POST['thfo_mailalert_details_1'] );
 		$details2 = sanitize_text_field( $_POST['thfo_mailalert_details_2'] );
 		$details3 = sanitize_text_field( $_POST['thfo_mailalert_details_3'] );
@@ -58,7 +88,7 @@
 		$details6 = sanitize_text_field( $_POST['thfo_mailalert_details_6'] );
 		$details7 = sanitize_text_field( $_POST['thfo_mailalert_details_7'] );
 		$details8 = sanitize_text_field( $_POST['thfo_mailalert_details_8'] );
-		
+
 		$pro_meta = array(
 			'details_1' => $details1,
 			'details_2' => $details2,
@@ -70,22 +100,24 @@
 			'details_8' => $details8,
 		);
 		$params   = array_merge( $params, $pro_meta );
-		
-		
+
+
 		return $params;
-		
+
 	}
-	
+
+add_action( 'wpcasama/pro/criteria', 'wpcasama_pro_metabox_alert' );
 	function wpcasama_pro_metabox_alert() {
+		global $post;
 		$details = wpsight_details();
-		$meta    = get_post_custom( $post_id->ID );
-		
-		
+		$meta    = get_post_custom( $post->ID );
+		$type = get_term_by( 'id', $meta['wpcasama_listing_type'][0], 'listing-type');
+		$type = $type->name;
 		?>
         <div class="wpcasama_search_criteria_right">
 			<?php foreach ( $details as $detail ) { ?>
 				<?php
-				
+
 				$key = array_key_exists( $detail['id'], $meta );
 				if ( $key ) { ?>
 
@@ -96,12 +128,13 @@
 								} ?> </p>
                         <div class="clear"></div>
                     </div>
-				
+
 				<?php }
 			} ?>
+	        <p><?php echo __('Listing Type: ', 'wpcasa-mail-alert-pro' ) . $type; ?></p>
 
         </div>
 		<?php
 	}
-	
-	add_action( 'wpcasama/pro/criteria', 'wpcasama_pro_metabox_alert' );
+
+
